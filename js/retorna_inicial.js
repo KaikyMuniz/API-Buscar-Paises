4575955d03533bdba137b1f22a45131c3c928e28
Kaiky Muniz <130181837+KaikyMuniz@users.noreply.github.com>
2023-12-17 01:17:18 -0300

    Create retorna_inicial.js

---
+++ js/retorna_inicial.js
@@ -0,0 +1,7 @@
+const btnSubmit = document.querySelector('#submit');
+
+btnSubmit.addEventListener('click', RetornaTelaInicial)
+
+function RetornaTelaInicial(e){
+    window.location.href = '../index.html';
+}
